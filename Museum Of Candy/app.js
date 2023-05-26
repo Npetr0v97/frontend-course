a85ef8261d1c08d19c72ca34eb8d761fad39384e
Npetr0v97 <69013216+Npetr0v97@users.noreply.github.com>
2023-05-26 14:40:40 +0300

    guessing game js

--- Museum Of Candy/app.js
+++ Museum Of Candy/app.js
@@ -1,23 +1,48 @@
 "use strict";
 
-
 const navbar = document.getElementById("mainNavbar");
 const navHeight = navbar.offsetHeight;
 
-console.log(navbar.offsetHeight);
-
-
 function scrollFunction() {
-    if (document.body.scrollTop > navHeight || document.documentElement.scrollTop > navHeight) {
-        navbar.classList.add("scrolled");
-    } else {
-        navbar.classList.remove("scrolled");
-    }
+  if (
+    document.body.scrollTop > navHeight ||
+    document.documentElement.scrollTop > navHeight
+  ) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
 }
 
 // window.onscroll = function () { scrollFunction() };
 
 window.addEventListener("scroll", scrollFunction);
 
-
 ////JS BASICS REFRESHER
+
+const maxNum = prompt("Welcome! Write your maximum number");
+const numberToGuess = Math.floor(Math.random() * maxNum) + 1;
+
+let numberOfGuesses = 0;
+let guess;
+
+console.log(numberToGuess);
+guess = prompt("Guess a number");
+
+while (true) {
+  numberOfGuesses++;
+  if (Number(guess) === numberToGuess) {
+    console.warn(
+      `That's correct! It took you ${
+        numberOfGuesses === 1
+          ? numberOfGuesses + " guess"
+          : numberOfGuesses + " guesses"
+      }.`
+    );
+    break;
+  } else if (Number(guess) > numberToGuess) {
+    guess = prompt("Try with a lower number.");
+  } else {
+    guess = prompt("Try with a higher number.");
+  }
+}
