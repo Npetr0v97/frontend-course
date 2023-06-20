c4865754cb63ab96e5805fb7343b914543b84726
Npetr0v97 <69013216+Npetr0v97@users.noreply.github.com>
2023-06-21 01:33:41 +0300

    pokemon project

---
+++ pokemon/app.js
@@ -0,0 +1,13 @@
+for (let i = 0; i < 150; i++) {
+  const html = `<div class="pokemon">
+    <img
+      src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+        i + 1
+      }.png"
+      alt=""
+    />
+    <span class="number">#${i + 1}</span>
+    </div>`;
+
+  document.querySelector("body").insertAdjacentHTML("beforeend", html);
+}
