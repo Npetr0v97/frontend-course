502bc70b0c2b6f6da3e239e4e01d842a41b640e0
Npetr0v97 <69013216+Npetr0v97@users.noreply.github.com>
2023-07-07 20:19:43 +0300

    npm tests

--- npm/app.js
+++ npm/app.js
@@ -1,1 +1,6 @@
-console.log("hello world");
+const jokes = require("give-me-a-joke");
+const colors = require("colors");
+
+jokes.getRandomDadJoke(function (joke) {
+  console.log(joke.rainbow);
+});
