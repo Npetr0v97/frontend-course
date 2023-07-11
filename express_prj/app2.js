6aa973713ddc9eb488adb3a25e045d9d1e527d6d
Npetr0v97 <69013216+Npetr0v97@users.noreply.github.com>
2023-07-12 02:26:51 +0300

    ejs exercise

---
+++ express_prj/app2.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path"); //module from node required for setting the proper path for the views for the templating engine
+
+const app = express();
+
+app.set("view engine", "ejs"); //setting the templating engine to ejs. No need to require ejs because with the set method ejs gets required behind the scenes
+app.set("views", path.join(__dirname, "/views")); //this sets the property of views meaning that it will search for the template located in __dirname/views which means the location of the js file and a directory called "views"
+
+app.get("/", (req, res) => {
+  res.render("home");
+  // renders the html (ejs file which is an html file). We only specify the name of the file and it automatically gets the file from the views folder. The views folder is mandatory
+});
+
+app.get("/random", (req, res) => {
+  const num = Math.floor(Math.random() * 10) + 1;
+  res.render("random", { rand: num }); //renders the random.ejs file with a variable rand = num
+});
+
+app.get("/r/:subreddit", (req, res) => {
+  const { subreddit } = req.params;
+  res.render("subreddit", { subreddit });
+});
+
+app.listen(3000, () => {
+  console.log("LISTENING ON PORT 3000");
+});
