cf57c5325322d06222748aa2ba535e51d905e396
Npetr0v97 <nikolay.petrov.petrov.97@gmail.com>
2023-07-28 13:39:49 +0300

    seeding

--- app_yelpcamp/app.js
+++ app_yelpcamp/app.js
@@ -3,14 +3,28 @@ const port = 3000;
 const path = require("path");
 const mongoose = require("mongoose");
 const methodOverride = require('method-override');
+const Campground = require("./models/campground");
 
 const app = express();
 
 //connecting to the database
-mongoose.connect('mongodb://127.0.0.1:27017/farmStand')
-    .then(() => console.log("Connection open!"))
-    .catch(err => { console.log("OH NO ERROR"); console.log(err); });
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    
 
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () =>console.log("Database connected"));
+
+
+app.get("/makecampground", async (req,res)=> {
+    const camp = new Campground({title: "My Backyard"});
+    // await camp.save();
+    res.render("home",{camp});
+})
 
 app.get("/", (req,res) => {
 
