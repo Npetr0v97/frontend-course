daabed530b9bf7f339c6cf32f7d977c4708f8ec5
Npetr0v97 <69013216+Npetr0v97@users.noreply.github.com>
2023-09-07 14:39:07 +0300

    w.e

--- react/shopping-list/src/ShoppingListItem.jsx
+++ react/shopping-list/src/ShoppingListItem.jsx
@@ -1,12 +1,11 @@
 export default function ShoppingListItem({ item }) {
+  const textStyle = {
+    color: item.completed ? "grey" : "red",
+    textDecoration: item.completed ? "line-through" : "",
+  };
   return (
     <>
-      <li
-        style={{
-          color: item.completed ? "grey" : "red",
-          textDecoration: item.completed ? "line-through" : "",
-        }}
-      >
+      <li style={textStyle}>
         {item.item} - {item.quantity}
       </li>
     </>
