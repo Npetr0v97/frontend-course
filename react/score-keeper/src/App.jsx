4359a7ed381fcc1d1649cf3f75abc02a20cd8103
Npetr0v97 <69013216+Npetr0v97@users.noreply.github.com>
2023-09-14 16:08:29 +0300

    scorekeeper game

---
+++ react/score-keeper/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Playerlist from "./PlayerList";
+
+function App() {
+  return (
+    <div>
+      <Playerlist numPlayers={5} />
+    </div>
+  );
+}
+
+export default App;
