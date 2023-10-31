9dd8fcfac702eb580a6ec6cf7c8d660e620a5a17
Npetr0v97 <69013216+Npetr0v97@users.noreply.github.com>
2023-10-31 20:12:55 +0200

    more react router

--- react-course/react-router/src/pages/BookList.jsx
+++ react-course/react-router/src/pages/BookList.jsx
@@ -1,5 +1,16 @@
+import { Link } from "react-router-dom";
+
 function BookList() {
-  return <h1>BookList</h1>;
+  return (
+    <>
+      <h1>BookList</h1>
+      <Link to="/books/1">Book 1</Link>
+      <br />
+      <Link to="/books/2">Book 2</Link>
+      <br />
+      <Link to="/books/new">New Book</Link>
+    </>
+  );
 }
 
 export default BookList;
