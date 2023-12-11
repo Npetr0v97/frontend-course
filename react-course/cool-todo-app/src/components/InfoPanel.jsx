4ef81a8cbece6fce5757c2e654ef93fa99acfeae
Npetr0v97 <69013216+Npetr0v97@users.noreply.github.com>
2023-12-11 02:30:02 +0200

    w/e

--- react-course/cool-todo-app/src/components/InfoPanel.jsx
+++ react-course/cool-todo-app/src/components/InfoPanel.jsx
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./InfoPanel.module.css";
 import { useSelector, useDispatch } from "react-redux";
 import {
   resetDummyData,
   setDummyData,
 } from "../../features/dummyData/dummyDataSlice";
+import axios from "axios";
 
 function InfoPanel() {
   const dummyData = useSelector((state) => state.dummyData.value);
   const dispatch = useDispatch();
-
   //api -> https://docs.zenquotes.io/zenquotes-documentation/
   //fetch via AXIOS
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await axios.get("https://zenquotes.io/api/quotes/");
+      console.log(data);
+    }
+    fetchData();
+  }, []);
 
   function onClickHandler() {
     console.log("Clicked");
